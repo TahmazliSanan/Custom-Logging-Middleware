edfc32afc1592b514c87340645633ea82907903a
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-12-23 11:26:22 +0400

    Create EmployeeUpdateDto

---
+++ CustomLoggingMiddleware/Dtos/EmployeeUpdateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CustomLoggingMiddleware.Dtos
+{
+    public class EmployeeUpdateDto
+    {
+        public string Position { get; set; } = string.Empty;
+        public double? Salary { get; set; }
+        public bool IsManager { get; set; }
+    }
+}
